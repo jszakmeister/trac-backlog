bf0835b9bb13fd84a520950ecb93148be031690e
Hiroyuki Wada <wadahiro@gmail.com>
2011-11-12 01:51:05 +0900

    add Content-Length in response header.

--- backlog/web_ui.py
+++ backlog/web_ui.py
@@ -289,6 +289,7 @@ class BacklogPlugin(Component):
         else:
             req.send_response(200)
         req.send_header('Content-Type', 'application/json')
+        req.send_header('Content-Length', len(data))
         req.end_headers()
         req.write(data)
 
@@ -342,6 +343,7 @@ class BacklogPlugin(Component):
         else:
             req.send_response(200)
         req.send_header('Content-Type', 'application/json')
+        req.send_header('Content-Length', len(data))
         req.end_headers()
         req.write(data)
 
@@ -420,6 +422,7 @@ class BacklogPlugin(Component):
             req.send_response(200)
 
         req.send_header('Content-Type', 'application/json')
+        req.send_header('Content-Length', len(data))
         req.end_headers()
         req.write(data)
         #raise RequestDone
