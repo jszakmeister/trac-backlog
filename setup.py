e0ed4b738e5d164726715f892f8a2a15f51dc775
John Szakmeister <john@szakmeister.net>
2013-03-01 04:09:59 -0500

    setup: reformat so that the indentation is correct

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ import backlog
 
 PACKAGE = 'TracBacklog'
 
-setup(name=PACKAGE,
-      version=backlog.get_version(),
-      packages=['backlog'],
-      package_data={
+setup(
+    name = PACKAGE,
+    version = backlog.get_version(),
+    packages = ['backlog'],
+    package_data = {
         'backlog': [
             'htdocs/css/*.css',
             'htdocs/img/*.png',
@@ -22,25 +23,24 @@ setup(name=PACKAGE,
             'htdocs/js/dijit/themes/tundra/tundra.css',
             'htdocs/js/dijit/themes/tundra/images/spriteArrows.png',
             'templates/*.html',
-        ]},
-      entry_points={
-          'trac.plugins': [
-              'backlog = backlog.web_ui',
-              'backlog_prefs = backlog.prefs',
-          ]
-      },
-      install_requires=[
+      ]},
+    entry_points = {
+        'trac.plugins': [
+            'backlog = backlog.web_ui',
+            'backlog_prefs = backlog.prefs',
+        ]
+    },
+    install_requires = [
         'simplejson>=2.0',
-        ],
-      author="John Szakmeister",
-      author_email="john@szakmeister.net",
-      description="Enables Trac to be used for managing your ticket backlog.",
-      long_description='''
+    ],
+    author = "John Szakmeister",
+    author_email = "john@szakmeister.net",
+    description = "Enables Trac to be used for managing your ticket backlog.",
+    long_description = '''
 Provides ticket backlog management in Trac.  It allows users to use
 drag-and-drop in their web browser to reorder tickets, and to assign tickets
 to a milestone.
 ''',
-      url="https://github.com/jszakmeister/trac-backlog",
-      license="Simplified BSD",
-      )
-
+    url = "https://github.com/jszakmeister/trac-backlog",
+    license = "Simplified BSD",
+)
